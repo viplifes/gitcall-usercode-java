71b0b24786ddc22f560936c77b631f58e6d562af
Dmitry Goncharov <dmitriy.goncharov@corezoid.com>
2022-12-06 19:07:04 +0200

    upgate gradle

--- usercode/src/main/java/com/corezoid/usercode/Usercode.java
+++ usercode/src/main/java/com/corezoid/usercode/Usercode.java
@@ -8,12 +8,12 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class Usercode implements UsercodeHandler<Map<String, String>, Map<String, String>> {
-
+    OkHttpClient client = new OkHttpClient();
     @java.lang.Override
     public Map<String, String> handle(Map<String, String> data) throws Exception {
-        OkHttpClient client = new OkHttpClient();
+
         Request request = new Request.Builder()
-                .url("https://www.liqpay.ua/api/ping")
+                .url("https://reqres.in/api/users?page=1")
                 .build();
 
         Response response = client.newCall(request).execute();

63013fa583bbe14d63eee30962016453f24cb83d
Dmitry Goncharov <dmitriy.goncharov@corezoid.com>
2022-12-06 21:14:54 +0200

    upgate gradle

--- usercode/src/main/java/com/corezoid/usercode/Usercode.java
+++ usercode/src/main/java/com/corezoid/usercode/Usercode.java
@@ -6,22 +6,22 @@ import java.util.Map;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
- import javax.mail.internet.InternetAddress;
+// import javax.mail.internet.InternetAddress;
 
 public class Usercode implements UsercodeHandler<Map<String, String>, Map<String, String>> {
-//    OkHttpClient client = new OkHttpClient();
+   OkHttpClient client = new OkHttpClient();
     @java.lang.Override
     public Map<String, String> handle(Map<String, String> data) throws Exception {
 
-//        Request request = new Request.Builder()
-//                .url("https://reqres.in/api/users?page=1")
-//                .build();
-//
-//        Response response = client.newCall(request).execute();
-//
-//        data.put("res", response.body().string());
+        Request request = new Request.Builder()
+                .url("https://reqres.in/api/users?page=1")
+                .build();
 
-        InternetAddress address = new InternetAddress("test@gmail.com");
+        Response response = client.newCall(request).execute();
+
+        data.put("res", response.body().string());
+
+//        InternetAddress address = new InternetAddress("test@gmail.com");
 
         data.put("address",  "123");
 
